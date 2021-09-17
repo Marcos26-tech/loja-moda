c34b44b53d66cb66fa25eb99f784b3c89b43103f
Marcos26-tech <rm88267@fiap.com.br>
2021-09-17 13:39:06 -0300

    ajustes na pag sobre

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -2,11 +2,20 @@ import React, {Component} from 'react';
 import {MenuItems} from "./MenuItems"
 import './Menu.css';
 
+
 class Menu extends Component {
+    state = { clicked: false}
+
+    handleclick = () =>{
+        this.setState({clicked: !this.state.clicked})
+    }
+
     render() {
         return(
             <nav className="menu">
-                <div className="menu-icon"></div>
+                <div className="menu-icon" onClick={this.handleclick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
                 <ul className="menu-itens">
                     {MenuItems.map((item, index) =>{
                         return (
