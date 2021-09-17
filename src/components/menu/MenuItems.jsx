98eb1bfbb23172bdd0238e509cd0bd76ee0945c3
Marcos26-tech <rm88267@fiap.com.br>
2021-09-17 09:30:33 -0300

    adiconado menu navbar

---
+++ src/components/menu/MenuItems.jsx
@@ -0,0 +1,27 @@
+export const MenuItems =[
+    {
+        title: 'Home',
+        url:'Home.jsx',
+        cName: 'menu',
+     },
+     {
+         title: 'Moda Masculina',
+         url:'ModaMasculina.jsx',
+         cName: 'menu, selecionado',
+      },
+      {
+         title: 'Moda Feminina',
+         url:'ModaFeminina.jsx',
+         cName: 'menu',
+      },
+      {
+         title: 'Moda Infatil',
+         url:'ModaInfantil.jsx',
+         cName: 'menu',
+      },
+      {
+         title: 'Sobre',
+         url:'Sobre.jsx',
+         cName: 'menu',
+      }
+]
