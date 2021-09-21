b05eb90db0d50e4fe921efb331befd9a23410e70
Marcos26-tech <rm88267@fiap.com.br>
2021-09-21 09:14:11 -0300

    add pag noda masculina

--- src/components/modamasculina/Styledmasculina.js
+++ src/components/modamasculina/Styledmasculina.js
@@ -17,23 +17,35 @@ export const StyledNovo = styled.div`
 `;
 
 export const StyledSection = styled.section`
-.camiseta {
-    display:block;
-    color: red;
-}
-.calca {
-    display:block;
-    color: blue;
-}
-
 
 `;
 
-export const Header = styled.div`
-  display: ${props => props.theme.display};
-`;
-
-export const StyledSpam = styled.div`
-    display: ${props => props.theme.display};
+export const StyledSpam = styled.section`
+    display:flex;
     
+    margin:1%;
+    width: 59%;
+    margin-left:20%;
+    justify-content: center;
+    align-items:center; 
+    div {
+        margin:1%;
+        justify-content: center;
+        align-items:center; 
+    }
+    img { 
+    position: relative;    
+    width: 100%;
+    height: 100%;  
+    }
+`;
+
+export const Img = styled.img`
+    padding-left:28rem;
+    margin:1%;
+`;
+
+export const ImgStyled = styled.img`
+    padding-left:21rem;
+    margin:1%;
 `;
