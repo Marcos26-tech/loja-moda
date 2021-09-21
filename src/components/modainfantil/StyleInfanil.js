bd6b07db09e55ea2183c9e87d3e7bcc5c7a97ca9
Marcos26-tech <rm88267@fiap.com.br>
2021-09-21 16:22:26 -0300

    ajuste paginas infantil

---
+++ src/components/modainfantil/StyleInfanil.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+//inicio style da pagina Feminina//
+export const Section = styled.section`
+  margin: 20px;
+  padding: 1px; 
+`;
+export const Title = styled.h1 ` 
+  margin: 2px;
+  padding: 1px;
+  font-size: 20px;
+  font-weight: bold;
+  color: rgb(12, 139, 1); 
+  display: flex;
+  text-align:center;
+  justify-content: center;
+`;
+
+export const StyledContainer = styled.section`
+  display: flex;
+  justify-content:space-evenly;
+  margin: 0;
+  margin-bottom:10px;
+`;
+
+//inicio style da pagina carrocelfeminina//
+export const StyledCarrocelContainer = styled.div`
+  height: 500px;
+  width: 27vw;
+  margin: 10px;
+  display: flex;
+  justify-content: center;
+  align-items:center;
+`;
+
+export const StyledImageContainer = styled.div`
+  width: 100%;
+  height: 100%;  
+`;
+
+export const StyledArrowContainer = styled.div`
+  font-size: 35px;
+  width:30px;
+  height:30px;
+  display: flex;
+  justify-content: center;
+  align-items:center;
+  border-radius:50%;
+  cursor: pointer;
+  &:hover{
+    background-color:#d9d9d9;
+  }
+`;
+
+export const StyledImage = styled.img`
+  width: 100%;
+  height:100%; 
+`;
