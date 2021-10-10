14bbee7ef6251b275ba084cf01667128510175ad
Marcos26-tech <rm88267@fiap.com.br>
2021-10-09 23:43:45 -0300

    fim

--- src/components/sobre/Sobre.jsx
+++ src/components/sobre/Sobre.jsx
@@ -5,7 +5,7 @@ import marcao from '../../img/grupo/MarcosVinicius.jpg';
 import akemi from '../../img/grupo/Akemi.jpg';
 import luan from '../../img/grupo/Luan.jpg';
 
-import { StyleContainer, Box, Paragrafo } from './StyleSobre'
+import { StyleContainer, Box, Paragrafo } from './StyledSobre'
 
 const Sobre = () => {
 
@@ -17,31 +17,31 @@ const Sobre = () => {
                 </div>
                 <Box>
                     <div>
-                        <img src={jojo} alt="Jonathan Felix"> <a href="https://www.linkedin.com/in/jonathan-f%C3%A9lix-12a9881a0/"></a></img>
+                        <img src={jojo} alt="Jonathan Felix"></img>
                         <h2>Jonathan Felix</h2>
                         <h3>Rm89352</h3>
                         <p>Banco de Dados</p>
                     </div>
                     <div>
-                        <img src={luan} alt="Laun de Oliveira"><a href="https://www.linkedin.com/in/luan-oliveira-brito-sousa-b01793192/"></a></img>
+                        <img src={luan} alt="Laun de Oliveira"></img>
                         <h2>Luan de Oliveira</h2>
                         <h3>Rm89352</h3>
                         <p>Banco de Dados</p>
                     </div>
                     <div>
-                        <img src={marcos} alt="Marcos Maciel"><a href="https://www.linkedin.com/in/markomaciell/"></a></img>
+                        <img src={marcos} alt="Marcos Maciel"></img>
                         <h2>Marcos Maciel</h2>
                         <h3>Rm88267</h3>
                         <p>AI & Chatbot</p>
                     </div>
                     <div>
-                        <img src={marcao} alt="Marcos Vinicius "><a href="https://www.linkedin.com/in/marcosvmferreira/"></a></img>
+                        <img src={marcao} alt="Marcos Vinicius "></img>
                         <h2>Marcos Vinicius</h2>
                         <h3>Rm86904</h3>
                         <p>Gestor de Projetos</p>
                     </div>
                     <div>
-                        <img src={akemi} alt="Akemi"><a href="https://www.linkedin.com/in/vitoria-akemi-tanimura-69561a1b2/"></a></img>
+                        <img src={akemi} alt="Akemi"></img>
                         <h2>Vitoria Akemi</h2>
                         <h3>Rm88077</h3>
                         <p>Front End</p>
